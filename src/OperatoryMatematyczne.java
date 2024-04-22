54812cfe39d68c545e0ee4d8e7976d104816aa96
“Marcin <marcin.leskiewicz@unitygroup.com>
2024-04-22 20:27:44 +0200

    Operatory matematyczne

---
+++ src/OperatoryMatematyczne.java
@@ -0,0 +1,19 @@
+// umożliwijają wykonanie podtawowych operacji matematycznych
+
+public class OperatoryMatematyczne {
+
+    public static void main(String[] args) {
+      int firstNumber = 4;
+      int secondNumber = 6;
+
+
+      int addition = firstNumber + secondNumber;
+      int subtraction = firstNumber - secondNumber;
+      int divison = firstNumber/secondNumber;
+      int mod = secondNumber%firstNumber;
+
+        System.out.println("Dodawanie: " + addition);
+        System.out.println("Odejmowanie: " + subtraction);
+        System.out.println("Dzielenie");
+    }
+}
