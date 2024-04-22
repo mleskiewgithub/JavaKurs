ca22680951793ebed14740d7b62f09fbcb8edfa0
“Marcin <marcin.leskiewicz@unitygroup.com>
2024-04-22 16:47:03 +0200

    Pierwszy program "Hello World" + komentarze

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+// pierwszy program + zastosowanie komentarzy (warianty) np. JIRA-VGPSUP-997
+public class HelloWorld {
+
+    //say Marcin
+    //metoda main - glowna metoda, w każdej aplikacji, która jest wykonywana podczas uruchomienia aplikacji
+    public static void main(String[] args) {
+        //wypisuje na ekranie konkretny ciag znakow
+        System.out.println("Hello Marcin!!!");
+        //System.out.println("How are you Marcin?");
+        /*
+        System.out.println("Where are you?");
+        System.out.println("Where do you live");
+        */
+    }
+}
