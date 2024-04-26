134ae1a4e9f9e1ebc21354547ac0b8dca1f44d36
“Marcin <marcin.leskiewicz@unitygroup.com>
2024-04-26 06:28:27 +0200

    Ad OperatoryPorownania

---
+++ src/OperatoryPorownania.java
@@ -0,0 +1,19 @@
+public class OperatoryPorownania {
+    public static void main(String[] args) {
+        int firstNumber = 4;
+        int secondNumber = 6;
+
+        boolean result = secondNumber > firstNumber;// true
+/*
+        System.out.println(result);
+        System.out.println(firstNumber == secondNumber); // false
+        System.out.println(firstNumber != secondNumber); // true
+        System.out.println(firstNumber > secondNumber); // false
+        System.out.println(firstNumber < secondNumber); // true
+        System.out.println(firstNumber >= secondNumber); // false
+        System.out.println(firstNumber <= secondNumber); // true
+*/
+        System.out.println("a > b: " + (firstNumber > secondNumber));
+
+    }
+}
